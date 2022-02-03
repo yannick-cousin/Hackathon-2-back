3af0534239126ecd99ef8fdf38901cb5431b1cb6
Mickaseg <mickael_seguin@hotmail.fr>
2022-02-03 17:49:10 +0100

    added delete route

--- src/routes/voyages.js
+++ src/routes/voyages.js
@@ -1,36 +1,36 @@
-const express = require('express');
+const express = require("express");
 // const { send } = require("express/lib/response");
-const connection = require('../helper/db.js');
+const connection = require("../helper/db.js");
 const Router = express.Router();
 
-Router.get('/home', (req, res) => {
-	const sql =
-		'SELECT id, name, destination, image, prix, evenement_date, notes FROM voyages';
+Router.get("/home", (req, res) => {
+  const sql =
+    "SELECT id, name, destination, image, prix, evenement_date, notes FROM voyages";
 
-	connection.query(sql, (err, result) => {
-		if (err) throw err;
-		if (result.length === 0) {
-			res.sendStatus(404);
-		  } else {
-			res.json(result);
-		  }
-	});
-	console.log('GET on /voyages/home');
-});
-
-Router.get('/reservation', (req, res) => {
-	const sql = 'SELECT * FROM voyages WHERE id=?';
-	const values = [req.body.id];
-
-	connection.query(sql, values, (err, result) => {
-		if (err) throw err;
-		if (result.length === 0) {
+  connection.query(sql, (err, result) => {
+    if (err) throw err;
+    if (result.length === 0) {
       res.sendStatus(404);
     } else {
       res.json(result);
     }
-	});
-	console.log('GET on /voyages/reservation');
+  });
+  console.log("GET on /voyages/home");
+});
+
+Router.get("/reservation", (req, res) => {
+  const sql = "SELECT * FROM voyages WHERE id=?";
+  const values = [req.body.id];
+
+  connection.query(sql, values, (err, result) => {
+    if (err) throw err;
+    if (result.length === 0) {
+      res.sendStatus(404);
+    } else {
+      res.json(result);
+    }
+  });
+  console.log("GET on /voyages/reservation");
 });
 
 module.exports = Router;
